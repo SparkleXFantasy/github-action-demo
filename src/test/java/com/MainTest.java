157778d38905c62118ed46ba01cfdb9c3577b91e
sxf <1559251498@qq.com>
2023-11-03 14:56:50 +0800

    add a new test case

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -9,8 +9,10 @@ class MainTest {
 	@Test
 	void test() {
 		Main main = new Main();
-		int result = main.add(1, 2);
-		assertEquals(2, result);
+		int result1 = main.add(1, 1);
+		assertEquals(2, result1);
+		int result2 = main.add(1, 2);
+		assertEquals(3, result2)
 	}
 
 }
